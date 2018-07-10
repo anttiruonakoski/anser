0605d0e8add9a10cc95d9964a04e451235c40488
Antti Ruonakoski <aruonakoski@gmail.com>
2018-07-10 19:09:44 +0300

    revert dependencies

--- config/deploy.php
+++ config/deploy.php
@@ -52,7 +52,7 @@ return [
         // Code and composer vendors are ready but nothing is built.
         'build' => [
             'npm:install',
-            'npm:development',
+            'npm:production',
         ],
         
         // Deployment is done but not live yet (before symlink)
